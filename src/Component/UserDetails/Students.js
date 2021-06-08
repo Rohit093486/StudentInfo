514be7f5f5db977c0bced22521ae28d5db759fa3
unknown <rohit.parihar@walkingtree.tech>
2021-06-08 11:18:03 +0530

    first commit

---
+++ src/Component/UserDetails/Students.js
@@ -0,0 +1,30 @@
+export const student=[
+    {
+        name: "Rohit Parihar",
+        email: "rohit.parihar@walkingtree.tech",
+        qualification: "MCA",
+        currentDateTime: "Mon Jun 07 2021",
+        id: 4
+      },
+      {
+        name: "Rohit",
+        email: "rohit.parihar@walkingtree.tech",
+        qualification: "M.Tech",
+        currentDateTime: "Mon Jun 07 2021",
+        id: 5
+      },
+      {
+        name: "komal",
+        email: "komal.parihar@walkingtree.tech",
+        qualification: "MCA",
+        currentDateTime: "Mon Jun 07 2021",
+        id: 6
+        },
+    {
+        name: "koma",
+        email: "koma.parihar@walkingtree.tech",
+        qualification: "MCA",
+        currentDateTime: "Mon Jun 07 2021",
+        id: 6
+      }
+]
